0d465bff8991953fdf86d6371837d43cc8400066
Angerleaf <raimok1998@gmail.com>
2019-02-20 14:41:56 +0200

    ddd

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 let btn = document.getElementById("btn");
 btn.onclick = () => console.log("Hello!");
+
+document.getElementById("btn").addEventListener("click", Hello()); 
+
+function Hello()
+{
+    console.log("Hello!");
+}
